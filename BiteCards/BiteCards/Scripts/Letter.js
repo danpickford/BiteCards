cb4cd8a5cc190e4731d72797f4c0ac30cefb577b
Daniel Pickford <email@danpickford.com>
2013-02-01 20:46:05 +1100

    Adding Javascript about to remove static content

---
+++ BiteCards/BiteCards/Scripts/Letter.js
@@ -0,0 +1,12 @@
+﻿function Letter(id, stopleft) {
+    this.id = id;
+    this.stopleft = stopleft;
+}
+
+Letter.prototype.actionMove = function () {
+    this.left = parseFloat($(id).css.left);
+    this.top = parseFloat($(id).css.top);
+    if (left > stopleft) {
+        $(id).css.left = (left - 3);
+    }
+};
